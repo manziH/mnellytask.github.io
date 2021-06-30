895bf09e3f5acdb5c280b80efb80108358e5fd96
manziH <67261223+manziH@users.noreply.github.com>
2021-07-01 00:35:08 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,38 @@
+function fetchData() {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => {
+      if (!response.ok) {
+        throw Error("ERROR");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      const html = data
+        .map((user) => {
+          return `<div class="user"> <p> ID:${user.id} </p> <p> Name:${user.name} </p> 
+            <p> Email:${user.email} </p>
+            <p> Username:${user.username} </p>
+             <p> Phone:${user.phone} </p> 
+            <p> Website:${user.website} </p> 
+            <button id="getpost" class="button" onclick="getPost()">Get User Post</button> </div>`;
+        })
+        .join("");
+      console.log(html);
+      document.querySelector("#app").insertAdjacentHTML("afterbegin", html);
+    })
+
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+fetchData();
+
+function getPost() {
+  url = fetch("https://jsonplaceholder.typicode.com/posts?userId=1")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
+
+getPost();
